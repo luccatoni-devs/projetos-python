a66808006103b1d9b3dd85783236da6dbf621daa
Lucca Toni <luccatoni.dev@gmail.com>
2025-07-30 14:27:55 -0300

    Add estruturas de dados

---
+++ Conhecendo a linguagem/declarando_listas.py
@@ -0,0 +1,11 @@
+frutas = ["laranja", "maçã", "uva"]
+print(frutas)
+
+letras = list("python")
+print(letras)
+
+numeros = list(range(10))
+print(numeros)
+
+carro = ["Ferrari", "F8", 430000, 2020, "São Paulo", True]
+print(carro)
